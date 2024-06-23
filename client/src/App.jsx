0d542e81320d581f63ab1b4465048f5574cef2aa
Aruneshwar Karthikeyan <aruneshwarkarthikeyan@gmail.com>
2024-06-24 04:00:30 +0530

    url update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ import AdminProtectedRoutes from './protectedroutes/AdminProtectedRoutes'
 // css import
 import './App.scss'
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://mern-stack-app-ymxk.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
